23bff811b36e75e4634f331a65bd6ccb05de59fc
toi-la-qanh <quocanh2003vn427@gmail.com>
2024-05-05 23:36:01 +0700

    add total cost to cart.php

--- html_backend/cart.php
+++ html_backend/cart.php
@@ -1,38 +1,62 @@
 <?php
-include "sql_connection.php";
+include "database/sql_connection.php";
 session_start();
-$conn = connect_to_sql();
+$db = new SQLConnect();
+$conn = $db->connect();
 $user_id = $_SESSION['user_id'];
 $get_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = '$user_id'");
 $get_cart->execute();
+$cost = 0;
 if ($get_cart->rowCount() > 0) {
     while ($cart = $get_cart->fetch()) {
-    ?>    
+        $cost += $cart['price']*$cart['quantity'];
+?>
         <form method="post" class="box" action="">
             <img src="images/<?php echo $cart['image']; ?>" alt="">
             <div class="name"><?php echo $cart['product_name']; ?></div>
             <div class="price"><?php echo $cart['price']; ?> đồng</div>
-            <div class="quantity">Số lượng:
-            <input type="number" min="1" name="quantity" value="<?php echo $cart['quantity']; ?>">
+            <div class="quantity">Số lượng: <?php echo $cart['quantity']; ?>
             </div>
+            <input type="hidden" name="prod_id" value="<?php echo $cart['product_id']; ?>">
             <input type="hidden" name="image" value="<?php echo $cart['image']; ?>">
             <input type="hidden" name="name" value="<?php echo $cart['product_name']; ?>">
             <input type="hidden" name="price" value="<?php echo $cart['price']; ?>">
             <input type="submit" value="Xoá khỏi giỏ hàng" name="remove_from_cart" class="btn">
         </form>
-    <?php
+<?php
     }
     ;
 }
 ;
-if(isset($_POST['remove_from_cart']))
-{
+if (isset($_POST['remove_from_cart'])) {
     $name = $_POST['name'];
-    $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE name = '$name' AND user_id = '$user_id' ");
+    $prod_id = $_POST['prod_id'];
+    $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE product_id = '$prod_id' AND user_id = '$user_id'");
     $select_cart->execute();
     if ($select_cart->rowCount() > 0) {
-        $conn->exec("DELETE FROM `cart` WHERE name = '$name' AND user_id = '$user_id'");
+        $conn->exec("DELETE FROM `cart` WHERE product_id = '$prod_id' AND user_id = '$user_id'");
         $message[] = 'Đã xoá sản phẩm khỏi giỏ hàng!';
     }
-};  
+}
+;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Giỏ hàng</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php
+
+    ?>
+    <div>Tổng tiền:
+        <?php echo $cost;?>
+    </div>
+</body>
+
+</html>
--- html_backend/user.php
+++ html_backend/user.php
@@ -1,12 +1,14 @@
 <?php
-    include "database/sql_connection.php";
-    $db = new SQLConnect();
-    $conn = $db->connect();
-    $query = $conn->prepare("SELECT name, email FROM `account_guest`"); 
-    $query->execute();
-    if ($query->rowCount() > 0) {
-        $user = $query->fetch();
-    };
+include "database/sql_connection.php";
+session_start();
+$db = new SQLConnect();
+$conn = $db->connect();
+$user_id = $_SESSION['user_id'];
+$query = $conn->prepare("SELECT name, email FROM `account_guest` WHERE id = '$user_id'");
+$query->execute();
+if ($query->rowCount() > 0) {
+    $user = $query->fetch();
+};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,16 +17,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tài khoản</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
     <div class="user-profile">
         <p> Tên:<span>
-            <?php echo $user['name']; ?>
-        </span></p>
+                <?php echo $user['name']; ?>
+            </span></p>
         <p> Email:<span>
-            <?php echo $user['email']; ?>
-        </span></p>
+                <?php echo $user['email']; ?>
+            </span></p>
     </div>
 </body>
 
